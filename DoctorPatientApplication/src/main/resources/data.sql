7e68851f64c9faf7cd95a8420398056bc7c0ace0
ZahidFarooqDar <79592620+ZahidFarooqDar@users.noreply.github.com>
2023-03-06 16:24:00 +0530

    Add files via upload

---
+++ DoctorPatientApplication/src/main/resources/data.sql
@@ -0,0 +1,11 @@
+INSERT
+INTO
+  tbl_doctor
+  (doctor_name, experience, specialized_in)
+VALUES
+  ('Dr. Zahid', 10, 'ortho'),
+  ('Dr. Amir', 8, 'oncology'),
+  ('Dr. Asrar', 12, 'cardiology'),
+  ('Dr. Umar', 15, 'haemology'),
+  ('Dr. Faheem', 9, 'General');
+
